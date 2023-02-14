03151994b0ed6621c570501d81b411bd6eb76c22
JhonatanWalterSen <jhonatanarizaq@gmail.com>
2023-02-14 01:44:45 -0500

    primer proyecto

---
+++ Creador_de_Nombres/cervezas.py
@@ -0,0 +1,6 @@
+# print('A\tB\t C\nD\tE\t F\nG\tH\t I')
+# print('Barra Normal: /\nBarra Invertida: \\')
+# print('Tu Nombre es ' + input('Dime tu Nombre: ') + " " + input('Dime tu Apellido: '))
+
+#Nombre de Cerveza (2 preguntas creativas para crear una marca) "Entre comillas" Saltos de linea dentro del codigo
+print('El Nombre de tu cerveza es: \n\"' + input('¿Tu ciudad favorita?')+input('¿Qué idioma te gustaría aprender?') + '\"')
