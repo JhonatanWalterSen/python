cfa198d6685b8e3b14184e64ae9ac2b1840440c7
JhonatanWalterSen <jhonatanarizaq@gmail.com>
2023-02-15 23:41:26 -0500

    strings

--- Analizador_de_texto/Teoria_metodos.py
+++ Analizador_de_texto/Teoria_metodos.py
@@ -20,3 +20,56 @@ print(res2)
 #rindex() busca de derecha a izquierda
 
 ## Extraer SubString
+texto= "ABCDEFGHI"
+fragmento = texto[2:5]
+#Etraer del indice 2 al 5
+print(fragmento)
+
+text = "Este es el texto de Jhonatan"
+resul = text
+
+print(resul)
+
+#transformar a mayusculas
+print(resul.upper())
+
+#Separar en elementos en una lista
+print(resul.split(" "))
+
+
+#join()
+
+a= "Aprender"
+b= "Python"
+c= "es"
+d= "genial"
+e= " ".join([a,b,c,d])
+print(e)
+
+#find() buscar un determinado caracter da resultado la posicion o -1
+
+conFind = e.find('x')
+print(conFind)
+
+
+#replace
+replaceResult = e.replace("Python", "C#")
+print(replaceResult)
+
+
+frase = 'Si la implementación es difícil de explicar, puede que sea una mala idea.'
+print(frase.replace("mala","buena").replace("difícil", "fácil"))
+
+nombre = "carina"
+print(nombre*5)
+
+#Salto de linea en los Strings
+poema="""Mil pequeños  
+    peces
+    blancos"""
+print(poema)
+
+print("peces" in poema)
+
+print("peces" not in poema)
+print(len(poema))
